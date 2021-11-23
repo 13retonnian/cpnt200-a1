1be770275b84b65d692f3b3db5a3fd1217c769e1
13retonnian <13retonnian@protonmail.com>
2021-11-22 17:20:08 -0700

    initial commit

---
+++ cpnt200-a1/pages/index.vue
@@ -0,0 +1,10 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <div>
+    <NuxtLink to="/second">Second Page</NuxtLink>
+    <nuxt-img src="/castle1.jpg" />
+  </div>
+</template>
---
+++ cpnt200-a1/pages/second.vue
@@ -0,0 +1,10 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <div>
+    <NuxtLink to="/">First Page</NuxtLink>
+    <nuxt-img src="/castle2.jpg" />
+  </div>
+</template>
